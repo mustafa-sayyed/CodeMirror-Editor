33c71ba649076e7d8fd4785901d0099f0a224dea
Mustafa Sayyed <162483528+mustafa-sayyed@users.noreply.github.com>
2025-12-06 11:09:29 +0530

    Update Editor.tsx

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -4,7 +4,65 @@ import { oneDark } from "@codemirror/theme-one-dark";
 import { basicSetup, EditorView } from "codemirror";
 import React, { useEffect, useRef } from "react";
 
-const initialText = "// Hello World \n// Write your code here... \n\nconsole.log('Made with Curiousity by Mustafa Sayyed')" + "\n".repeat(23);
+const initialText = "// Hello World \n// Write your code here... \n\nconsole.log('Made with Curiousity by Mustafa Sayyed')
+
+
+#include <stdio.h> #include <stdlib.h>
+
+struct Node {
+
+int data;
+
+struct Node* prev; struct Node* next;
+
+struct Node* createNode(int value) {
+
+struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+
+newNode->data = value;
+
+newNode->prev = NULL;
+
+newNode->next = NULL;
+
+return newNode;
+
+struct Node* insertBeginning(struct Node* head, int value) { struct Node* newNode = createNode(value);
+
+newNode->next = head;
+
+if (head != NULL) {
+
+head->prev = newNode;
+}
+
+head = newNode:
+
+return head:
+
+void display(struct Node* head) {
+
+struct Node* temp = head;
+
+while (temp!= NULL) {
+
+printf("%d ", temp->data);
+
+temp = temp->next; // single pointer movement
+
+int main() {
+
+struct Node* head = NULL;
+
+head = insertBeginning(head, 30); head = insertBeginning(head, 20); head = insertBeginning(head, 10);
+
+printf("Doubly Linked List: "); display(head);
+
+return 0;
+}
+
+
+" + "\n".repeat(5);
 
 const Editor: React.FC = () => {
   const editorRef = useRef<HTMLDivElement | null>(null);
